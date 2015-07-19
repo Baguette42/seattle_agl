d981415c1a788ebafc6804188ce3763bc1f9b444
Baguette42 <guillaume.moizan@gmail.com>
2015-07-19 21:17:51 +0200

    First commit

---
+++ AGL/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace AGL
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loadBesoins_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".xlsx";
+            dlg.Filter = "Excel file (.xlsx)|*.xlsx";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+            }
+
+        }
+
+        private void loadMCD_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".xml";
+            dlg.Filter = "XML file (.xml)|*.xml";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+            }
+        }
+
+        private void loadUseCase_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".xmi";
+            dlg.Filter = "XMI file (.xmi)|*.xmi";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+            }
+        }
+
+        private void validatePasserelleA_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            WordDocumentWriter.CreateSTB(dialog.SelectedPath);
+
+            System.Windows.Forms.MessageBox.Show("STB générée dans le dossier " + dialog.SelectedPath);
+        }
+
+    }
+}
---
+++ AGL/WordDocumentWriter.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.Office.Interop.Word;
+
+namespace AGL
+{
+    public static class WordDocumentWriter
+    {
+
+         //Create document method
+        public static void CreateSTB(string path)
+        {
+            try
+            {
+                //Create an instance for word app
+                Microsoft.Office.Interop.Word.Application winword = new Microsoft.Office.Interop.Word.Application();
+
+                //Set animation status for word application
+                winword.ShowAnimation = false;
+
+                //Set status for word application is to be visible or not.
+                winword.Visible = false;
+
+                //Create a missing variable for missing value
+                object missing = System.Reflection.Missing.Value;
+
+                //Create a new document
+                Microsoft.Office.Interop.Word.Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+                //Add header into the document
+                foreach (Microsoft.Office.Interop.Word.Section section in document.Sections)
+                {
+                    //Get the header range and add the header details.
+                    Microsoft.Office.Interop.Word.Range headerRange = section.Headers[Microsoft.Office.Interop.Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                    headerRange.Fields.Add(headerRange, Microsoft.Office.Interop.Word.WdFieldType.wdFieldPage);
+                    headerRange.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    headerRange.Font.ColorIndex = Microsoft.Office.Interop.Word.WdColorIndex.wdBlue;
+                    headerRange.Font.Size = 10;
+                    headerRange.Text = "Header text goes here";
+                }
+
+                //Add the footers into the document
+                foreach (Section wordSection in document.Sections)
+                {
+                    //Get the footer range and add the footer details.
+                    Microsoft.Office.Interop.Word.Range footerRange = wordSection.Footers[Microsoft.Office.Interop.Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                    footerRange.Font.ColorIndex = Microsoft.Office.Interop.Word.WdColorIndex.wdDarkRed;
+                    footerRange.Font.Size = 10;
+                    footerRange.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    footerRange.Text = "Footer text goes here";
+                }
+
+                //adding text to document
+                document.Content.SetRange(0, 0);
+                document.Content.Text = "This is test document " + Environment.NewLine;
+
+                //Add paragraph with Heading 1 style
+                Microsoft.Office.Interop.Word.Paragraph para1 = document.Content.Paragraphs.Add(ref missing);
+                object styleHeading1 = "Titre 1";
+                para1.Range.set_Style(ref styleHeading1);
+                para1.Range.Text = "Para 1 text";
+                para1.Range.InsertParagraphAfter();
+
+                //Add paragraph with Heading 2 style
+                Microsoft.Office.Interop.Word.Paragraph para2 = document.Content.Paragraphs.Add(ref missing);
+                object styleHeading2 = "Titre 2";
+                para2.Range.set_Style(ref styleHeading2);
+                para2.Range.Text = "Para 2 text";
+                para2.Range.InsertParagraphAfter();
+
+                //Create a 5X5 table and insert some dummy record
+                Microsoft.Office.Interop.Word.Table firstTable = document.Tables.Add(para1.Range, 5, 5, ref missing, ref missing);
+
+                firstTable.Borders.Enable = 1;
+                foreach (Microsoft.Office.Interop.Word.Row row in firstTable.Rows)
+                {
+                    foreach (Microsoft.Office.Interop.Word.Cell cell in row.Cells)
+                    {
+                        //Header row
+                        if (cell.RowIndex == 1)
+                        {
+                            cell.Range.Text = "Column " + cell.ColumnIndex.ToString();
+                            cell.Range.Font.Bold = 1;
+                            //other format properties goes here
+                            cell.Range.Font.Name = "verdana";
+                            cell.Range.Font.Size = 10;
+                            //cell.Range.Font.ColorIndex = WdColorIndex.wdGray25;                            
+                            cell.Shading.BackgroundPatternColor = Microsoft.Office.Interop.Word.WdColor.wdColorGray25;
+                            //Center alignment for the Header cells
+                            cell.VerticalAlignment = Microsoft.Office.Interop.Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                            cell.Range.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                        }
+                        //Data row
+                        else
+                        {
+                            cell.Range.Text = (cell.RowIndex - 2 + cell.ColumnIndex).ToString();
+                        }
+                    }
+                }
+
+                //Save the document
+                object filename = @"";
+                //if a path is defined, filename = selected path + STB.docx
+                if (path != null && !path.Equals(""))
+                {
+                    string temp = path + "\\STB.docx";
+                    filename = @temp;
+                }
+                else
+                    //else, the following default emplacement is used
+                    filename = @"C:\Users\Public\Documents\STB.docx";
+
+                document.SaveAs2(ref filename);
+                document.Close(ref missing, ref missing, ref missing);
+                document = null;
+                winword.Quit(ref missing, ref missing, ref missing);
+                winword = null;
+                //MessageBox.Show("Document created successfully !");
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
