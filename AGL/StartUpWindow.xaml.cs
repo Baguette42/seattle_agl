d754188d8323bed047965d8af6fd7a9d18d1a4a7
Baguette42 <guillaume.moizan@gmail.com>
2015-09-08 12:15:19 +0200

    Added start up window to create a project

---
+++ AGL/StartUpWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AGL
+{
+    /// <summary>
+    /// Logique d'interaction pour StartUpWindow.xaml
+    /// </summary>
+    public partial class StartUpWindow : Window
+    {
+        public StartUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void yes_Click(object sender, RoutedEventArgs e)
+        {
+            string netbeansPath = "C:\\Program Files\\NetBeans 8.0.2\\bin\\netbeans.exe";
+            Process createProjectProcess = Process.Start(netbeansPath);
+            createProjectProcess.WaitForExit();
+
+            LoadProject loadProjectWindow = new LoadProject();
+            loadProjectWindow.Show();
+            this.Close();
+        }
+
+        private void no_Click(object sender, RoutedEventArgs e)
+        {
+            LoadProject loadProjectWindow = new LoadProject();
+            loadProjectWindow.Show();
+            this.Close();
+        }
+    }
+}
